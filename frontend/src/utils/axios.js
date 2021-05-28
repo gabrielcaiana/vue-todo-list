f07ae05f32f55786e7a3a9cf62662d0d5e6c1842
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-28 00:00:35 -0300

    atualizando o axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3333',
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json',
-  },
+  baseURL: 'http://localhost:3333'
 });
