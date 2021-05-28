6f043c86c95f5f51fc8b4b903aa28bc191ab887f
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-27 23:15:11 -0300

    configurando controller de todo

--- api/app/Controllers/Http/TodosController.ts
+++ api/app/Controllers/Http/TodosController.ts
@@ -1,4 +1,34 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Todo from 'App/Models/Todo'
 
 export default class TodosController {
+  public async index({}: HttpContextContract) {
+    const todos = await Todo.query().orderBy('id')
+    return todos
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['text', 'done'])
+    const todo = await Todo.create(data)
+    return todo
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id)
+    return todo
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id)
+    const data = await request.only(['text', 'done'])
+
+    todo.merge(data)
+
+    return todo
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const todo = await Todo.findOrFail(params.id)
+    await todo.delete()
+  }
 }
